2a75d6a822977b8d82078070f919fe88935e8b06
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-07 04:52:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,8 @@ aoh = [
 DON_G=>{name:"Don Gately", occupation:"Live-in Staff"}, 
 JOELLE_VD=>{name:"Joelle van Dyne", occupation:"Radio Personality"}, 
 PAT_M=>{name:"Pat Monteseian", occupation:"Staff"},
-KATE_G=>, BRUCE_G]
+KATE_G=>{name:"Kate Gompert", occupation:"None"}, 
+BRUCE_G=>]
 end
 
 def literal_aoh
